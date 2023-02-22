6b185fc9fcf6512d0d8a64e8420d88ffb3dfae53
sayakaliyah <116573858+sayakaliyah@users.noreply.github.com>
2023-02-22 22:46:31 +0800

    NewModels.php

---
+++ ci4/app/Models/NewModels.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class NewsModel extends Model
+{
+    protected $table = 'news';
+    protected $table = 'sphernandez_news';
+	    public function getNews($slug = false)
+    {
+        if ($slug === false) {
+            return $this->findAll();
+        }
+
+        return $this->where(['slug' => $slug])->first();
+    }
+}
