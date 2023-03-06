445c423fda15ca5686eae5cee5aa0c404832e995
sayakaliyah <116573858+sayakaliyah@users.noreply.github.com>
2023-03-06 23:25:24 +0800

    Update home.php

--- ci4/app/Views/pages/home.php
+++ ci4/app/Views/pages/home.php
@@ -13,6 +13,7 @@ body {
   margin: auto;
   text-align: center;
   background-color:white;
+  color: mediumorchid;
 }
 
 button {
@@ -134,8 +135,6 @@ if (new Date().getHours() < 18) {
   </div>
 </div>
 
-<p><i>images are all from pinterest!</i></p>
-
   <div style="margin: 30px 0; margin-top: 10px;"> 
 	</div>
 </div>
@@ -145,128 +144,12 @@ if (new Date().getHours() < 18) {
 
 
 <div class="card">
-   <?php
-// define variables and set to empty values
-$nameErr = $emailErr = $genderErr = $websiteErr = "";
-$name = $email = $gender = $comment = $website = "";
+ <a href="http://apcwebprog.csf.ph/~sphernandez/lab3/ci4/public/guest/create" style="margin-right: 10px;"><h1><button><b><i>GUEST FORM</i></b></button></h1></a>
+ 
+ <a href="http://apcwebprog.csf.ph/~sphernandez/lab3/ci4/public/guest/" style="margin-right: 10px;"><h1><button ><b><i>GUEST BOOK</i></b></button></h1></a>
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed";
-    }
-  }
-  
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format";
-    }
-  }
-    
-  if (empty($_POST["website"])) {
-    $website = "";
-  } else {
-    $website = test_input($_POST["website"]);
-    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-      $websiteErr = "Invalid URL";
-    }
-  }
+ <a href="https://pin.it/6EQrKjI" style="margin-right: 10px;"><h1><button ><b><i>IMAGES SOURCE</i></b></button></h1></a>
 
-  if (empty($_POST["comment"])) {
-    $comment = "";
-  } else {
-    $comment = test_input($_POST["comment"]);
-  }
-
-  if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
-  } else {
-    $gender = test_input($_POST["gender"]);
-  }
-}
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-?>
-
- <h1><button><b><i>GUEST FORM</i></b></button></h1>
-<p><span class="error">* required field</span></p>
-<form method="post" action="/~sphernandez/lab3/ci4/public/home">  
-  Name: <input type="text" name="name" value="<?php echo $name;?>">
-  <span class="error">* <?php echo $nameErr;?></span>
-  <br><br>
-  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-  <span class="error">* <?php echo $emailErr;?></span>
-  <br><br>
-  Website: <input type="text" name="website" value="<?php echo $website;?>">
-  <span class="error"><?php echo $websiteErr;?></span>
-  <br><br>
-  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-  <br><br>
-  Gender:
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
-  <span class="error">* <?php echo $genderErr;?></span>
-  <br><br>
-  <input type="submit" name="submit" value="Submit">  
-</form>
-
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $website;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $gender;
-?>
-
- <h1><button><b><i>GUEST LIST</i></b></button></h1>
-<?php
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") 
-{
-        $servername = "192.168.150.213";
-        $username = "webprogss211";
-        $password = "fancyR!ce36";
-        $dbname = "webprogss211";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "INSERT INTO sphernandez_myguests ( name, email, website, comment, gender)
-VALUES ('$name', '$email', '$website', '$comment', '$gender')";
-
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-$conn->close();
-}
-
-?>
 </div>
 
 
